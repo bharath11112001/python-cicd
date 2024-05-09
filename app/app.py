7660916774b90c70ab9ea61bbceb701a16ec5e01
bharath11112001 <162092691+bharath11112001@users.noreply.github.com>
2024-05-09 14:25:53 +0530

    Update app.py
    updated my application

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Welcome to Devops and CICD Jenkins app'
+    return 'Welcome to Devops and CICD Jenkins application'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
